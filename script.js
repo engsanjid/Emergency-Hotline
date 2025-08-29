f2d9b359c991da0d126022ba04a210c29bc2d8e1
engsanjid <mdsanjidi36@gmail.com>
2025-08-29 21:49:02 +0600

    access element

---
+++ script.js
@@ -0,0 +1,14 @@
+// Navbar counts
+let heartCount = 0;
+let copyCount = 0;
+let coinCount = 100;
+
+// HTML elements
+const heartCounter = document.getElementById("heartCount");
+const copyCounter = document.getElementById("copyCount");
+const coinCounter = document.getElementById("coinCount");
+
+// Call History Section
+const historyList = document.getElementById("historyList");
+const clearHistoryBtn = document.getElementById("clearHistory");
+
